2a739741eef380f461f4bc6e5eddbeb7cf679554
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-11-20 14:32:10 -0800

    moving node code to dedicated folder

--- node/app.js
+++ node/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const client = require('prom-client');
 const collectDefaultMetrics = client.collectDefaultMetrics;
+const projectID = 'yuri-next2019'; //TODO - make this a config setting
+
+//counter metric
+const counter = new client.Counter({
+    name: 'example_counter',
+    help: 'counter_help'
+});
 
 // Probe every 5th second.
 collectDefaultMetrics({ timeout: 5000 });
 
-const projectID = 'yuri-next2019'; //TODO - make this a config setting
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -14,7 +21,8 @@ function getRandomInt(max) {
 
 app.get('/', (req, res) => {
     console.log('request made');
-    res.send('!');
+    counter.inc();
+    res.send('current counter value is: ' + counter.hash_map.value);
 })
 
 app.listen(8080, () => console.log(`Example app listening on port 8080!`))
