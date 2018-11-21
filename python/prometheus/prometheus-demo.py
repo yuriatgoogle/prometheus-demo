30282dc65f0bdab6728de293c812ef0dfd7772f8
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-11-21 13:59:09 -0800

    creating metrics in python works

--- python/prometheus/prometheus-demo.py
+++ python/prometheus/prometheus-demo.py
@@ -1,19 +1,15 @@
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Summary, Gauge, Counter
 import random
 import time
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
-# Decorate function with metric.
-@REQUEST_TIME.time()
-def process_request(t):
-    """A dummy function that takes some time."""
-    time.sleep(t)
+# Create metrics
+c = Counter('incrementing_counter', 'incrementing value')
+g = Gauge('random_value', 'randomly set gauge value')
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
     start_http_server(8000)
     # Generate some requests.
-    while True:
-        process_request(random.random())
+    while True: # on every request
+        c.inc() # increment the counter
+        g.set(random.random()) # randomize the gauge
