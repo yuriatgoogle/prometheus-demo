9ec7c1ccaa71aab9d49c10029f18dac5232c3916
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2018-11-30 15:10:59 -0800

    adding more metrics

--- go/main.go
+++ go/main.go
@@ -1,21 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-func main() {
+// create gauge metric
+var (
+	go_random_value = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "go_random_value",
+		Help: "randomly generated value in Go",
+	})
+)
 
+// create counter metric
+var (
+	pageHits = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "go_main_page_hits",
+		Help: "The total number of times the main page was hit",
+	})
+)
+
+func main() {
 	log.Printf("main function")
+	pageHits.Inc()
 	http.HandleFunc("/", handle)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello!")
+	go_random_value.Set(rand.Float64()) //
+
 }
