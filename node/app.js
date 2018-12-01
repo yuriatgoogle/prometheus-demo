9a69355f2f3111363516dd08d576a58afb72c278
Yuri Grinshteyn <ygrinshteyn@google.com>
2018-12-01 17:50:14 +0000

    node works!

--- node/app.js
+++ node/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const client = require('prom-client');
-const collectDefaultMetrics = client.collectDefaultMetrics;
+const prometheus = require('prom-client');
+const collectDefaultMetrics = prometheus.collectDefaultMetrics;
 const projectID = 'yuri-next2019'; //TODO - make this a config setting
 
 //counter metric
-const counter = new client.Counter({
+const counter = new prometheus.Counter({
     name: 'example_counter',
     help: 'counter_help'
 });
@@ -13,16 +13,15 @@ const counter = new client.Counter({
 // Probe every 5th second.
 collectDefaultMetrics({ timeout: 5000 });
 
-
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-
 app.get('/', (req, res) => {
     console.log('request made');
     counter.inc();
-    res.send('current counter value is: ' + counter.hash_map.value);
+    res.send("home page");
 })
+
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', prometheus.register.contentType)
+    res.end(prometheus.register.metrics())
+  })
 
 app.listen(8080, () => console.log(`Example app listening on port 8080!`))
