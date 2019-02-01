f37d565174f7b64f755b624a52c5d8c515050ef7
Yuri Grinshteyn <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2019-02-01 10:54:11 -0800

    cleaning up go code

--- go/main.go
+++ go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
@@ -12,29 +13,20 @@ import (
 
 // create gauge metric
 var (
-	go_random_value = promauto.NewGauge(prometheus.GaugeOpts{
+	goRandomValue = promauto.NewGauge(prometheus.GaugeOpts{
 		Name: "go_random_value",
 		Help: "randomly generated value in Go",
 	})
 )
 
-// create counter metric
-var (
-	pageHits = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "go_main_page_hits",
-		Help: "The total number of times the main page was hit",
-	})
-)
-
 func main() {
 	log.Printf("main function")
-	pageHits.Inc()
 	http.HandleFunc("/", handle)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	go_random_value.Set(rand.Float64()) //
-
+	goRandomValue.Set(rand.Float64()) //
+	fmt.Fprintf(w, "Hello")
 }
