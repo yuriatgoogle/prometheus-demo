7ff08311db47037e812f2629e3ea8aa5adae2b5d
rajatparida86 <rajatparida86@gmail.com>
2020-07-16 17:17:17 +0200

    Implement sample go app with prom instrumentation

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module github.com/rajatparida86/prometheus-demo/go
+
+go 1.13
+
+require github.com/prometheus/client_golang v1.7.1
--- go/main.go
+++ go/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -13,9 +14,27 @@ import (
 
 // create gauge metric
 var (
-	goRandomValue = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "go_random_value",
-		Help: "randomly generated value in Go",
+	// total requests counter
+	goRequestCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "go_requests",
+		Help: "total go requests",
+	})
+	// total failed requests counter
+	goFailedRequestCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "go_failed_requests",
+		Help: "total failed go requests",
+	})
+	// go successful request latency histogram
+	goSuccessLatencyHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name:    "go_success_request_latency",
+		Help:    "go successful request latency by path",
+		Buckets: []float64{100, 400},
+	})
+	// go failed request latency histogram
+	goFailedLatencyHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+		Name:    "go_failed_request_latency",
+		Help:    "go failed request latency by path",
+		Buckets: []float64{100, 400},
 	})
 )
 
@@ -27,6 +46,32 @@ func main() {
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
-	goRandomValue.Set(rand.Float64()) //
-	fmt.Fprintf(w, "Hello")
+	// record incoming request time
+	requestReceivedTime := time.Now()
+	// increment total request counter
+	goRequestCounter.Inc()
+	rand.Seed(time.Now().Unix())
+
+	// sleep randomly before failing
+	if rand.Intn(10) == 1 {
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
+		requestFailedLatency := time.Now().Sub(requestReceivedTime)
+		// increment failed request counter
+		goFailedRequestCounter.Inc()
+		// record failed latency
+		goFailedLatencyHistogram.Observe(float64(requestFailedLatency))
+
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "failed in %v ms", requestFailedLatency.Milliseconds())
+	} else {
+		// sleep randomly before being successful
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(500)))
+
+		// collect latency histogram
+		requestSuccessLatency := time.Now().Sub(requestReceivedTime)
+		goSuccessLatencyHistogram.Observe(float64(requestSuccessLatency))
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "success in %v ms", requestSuccessLatency.Milliseconds())
+	}
 }
